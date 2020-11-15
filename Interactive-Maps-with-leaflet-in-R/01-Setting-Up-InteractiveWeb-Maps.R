4740df340295a7b6c65ee05025537da48a77ec6a
Vitor Borges Jr <vntborgesjr@gmail.com>
2020-11-15 00:23:27 -0300

    Create 01-Setting-Up-InteractiveWeb-Maps.R

---
+++ Interactive-Maps-with-leaflet-in-R/01-Setting-Up-InteractiveWeb-Maps.R
@@ -0,0 +1,7 @@
+# --------------------------------------------------- 
+# Interactive Maps with leaflet in R - Setting Up Interactive Web Maps 
+# 15 nov 2020 
+# VNTBJR 
+# --------------------------------------------------- 
+#
+# Introduction to leaflet  -------------------------------------------
